be7541be3b9da3198e286ecb22e04ec413f48b5a
crubinowicz <charlie.rubinowicz@kellogg.northwestern.edu>
2022-02-22 02:47:35 +0000

    updated links

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,19 @@
-class PostsController < ApplicationController
-    def index
-        @posts = Post.all
+# @place = Place.find(params["place_id"])
+        # @post.place_id = @place.id
+        class PostsController < ApplicationController
+          def index
+              @posts = Post.all
+            end
+      
+            def new
+              @post = Post.new
+              # @place = Place.find(params["place_id"])
+              # @post.place_id = @place.id
+            end
+          
+            def create
+              @post = Post.new(params["post"])
+              @post.save
+              redirect_to "/posts"
+            end
       end
-    
-      def new
-        @post = Post.new
-      end
-    
-      def create
-        @post = Post.new(params["post"])
-        @post.save
-        redirect_to "/posts"
-      end
-end
